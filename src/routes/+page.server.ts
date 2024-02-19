29b2f692233efe08e3265e53ea498c3b4931339b
syedkhairi <syedkhairi@yahoo.com>
2024-02-19 21:11:54 +0000

    Initial commit

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,20 @@
+import { superValidate, message } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { fail } from '@sveltejs/kit';
+import { schema } from './schema.js';
+
+export const load = async () => {
+	const form = await superValidate(zod(schema));
+	return { form };
+};
+
+export const actions = {
+	default: async ({ request }) => {
+		const form = await superValidate(request, zod(schema));
+		console.log(form);
+
+		if (!form.valid) return fail(400, { form });
+
+		return message(form, 'Form posted successfully!');
+	}
+};
